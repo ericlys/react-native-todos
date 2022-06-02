6182f5f1a1b5e57fee6251f90a17b85ec0b4eff0
Ericlys Moreira <ericlysm@gmail.com>
2022-06-02 17:24:03 -0300

    feat: add, remove and toggle task done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,27 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const taskToUpdate = tasks.find(task => task.id === id);
+
+    const updatedTasks = tasks.map( task =>
+      task.id === id ? {...task, done:!taskToUpdate?.done }: task
+    )
+    
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id))
   }
 
   return (
